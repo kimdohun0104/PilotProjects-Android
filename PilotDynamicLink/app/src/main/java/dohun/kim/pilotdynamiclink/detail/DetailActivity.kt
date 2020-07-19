ace51dc7e86d0f0a90989cf9e3f81fa0b057831d
kimdohun0104 <dikolight203@gmail.com>
2020-07-19 16:48:01 +0900

    [UPDATE] Dynamic link initial commit Dynamic link setting finished

---
+++ PilotDynamicLink/app/src/main/java/dohun/kim/pilotdynamiclink/detail/DetailActivity.kt
@@ -0,0 +1,23 @@
+package dohun.kim.pilotdynamiclink.detail
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.dynamiclinks.FirebaseDynamicLinks
+import dohun.kim.pilotdynamiclink.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+
+        FirebaseDynamicLinks.getInstance()
+            .getDynamicLink(intent)
+            .addOnSuccessListener {
+                Log.d("DEBUGLOG", it.link.toString())
+            }
+            .addOnFailureListener {
+                Log.d("DEBUGLOG", it.message.toString())
+            }
+    }
+}
---
+++ PilotDynamicLink/app/src/main/java/dohun/kim/pilotdynamiclink/main/MainActivity.kt
@@ -0,0 +1,14 @@
+package dohun.kim.pilotdynamiclink.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import dohun.kim.pilotdynamiclink.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
