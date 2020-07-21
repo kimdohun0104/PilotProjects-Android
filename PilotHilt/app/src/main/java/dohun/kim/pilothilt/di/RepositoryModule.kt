eaadd590f623a3a1fb48e112149c8a4a87770b92
kimdohun0104 <dikolight203@gmail.com>
2020-07-21 14:15:36 +0900

    ActivityScope -> ActivityRetainedScope

--- PilotHilt/app/src/main/java/dohun/kim/pilothilt/di/RepositoryModule.kt
+++ PilotHilt/app/src/main/java/dohun/kim/pilothilt/di/RepositoryModule.kt
@@ -4,15 +4,17 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.components.ActivityRetainedComponent
+import dagger.hilt.android.scopes.ActivityRetainedScoped
 import dagger.hilt.android.scopes.ActivityScoped
 import dohun.kim.pilothilt.data.UserRepository
 import dohun.kim.pilothilt.data.UserRepositoryImpl
 
 @Module
-@InstallIn(ActivityComponent::class)
+@InstallIn(ActivityRetainedComponent::class)
 abstract class RepositoryModule {
 
     @Binds
-    @ActivityScoped
+    @ActivityRetainedScoped
     abstract fun bindsUserRepository(userRepositoryImpl: UserRepositoryImpl): UserRepository
 }
